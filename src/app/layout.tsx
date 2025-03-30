c336fac43c54e713408a37bbf460589e76237eba
kleeedolinux <nicolasketion@gmail.com>
2025-03-30 12:46:50 -0300

    metadata localization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { I18nProvider } from "./i18n/i18nContext";
+import enLocale from "./i18n/locales/en.json";
+import ptLocale from "./i18n/locales/pt.json";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,26 +11,43 @@ export const viewport: Viewport = {
   themeColor: "#6441a5"
 };
 
-export const metadata: Metadata = {
-  title: "Emoto - Twitch Emote Guessing Game",
-  description: "Emoto - Jogo de adivinhação de emotes da Twitch. Teste seu conhecimento de emotes de seus streamers favoritos!",
-  keywords: "Twitch, emotes, game, emote guessing, BTTV, 7TV, Twitch emotes, streaming",
-  authors: [{ name: "Júlia Klee" }],
-  metadataBase: new URL("https://emoto.juliaklee.wtf"),
-  openGraph: {
-    type: "website",
-    url: "https://emoto.juliaklee.wtf/",
-    title: "Emoto - Twitch Emote Guessing Game",
-    description: "Teste seu conhecimento de emotes da Twitch neste divertido jogo de adivinhação!",
-    images: ["/img/Emoto-Background.png"],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Emoto - Twitch Emote Guessing Game",
-    description: "Teste seu conhecimento de emotes da Twitch neste divertido jogo de adivinhação!",
-    images: ["/img/Emoto-Background.png"],
-  },
+type Locale = 'en' | 'pt';
+const resources = {
+  en: enLocale,
+  pt: ptLocale
 };
+
+export async function generateMetadata(): Promise<Metadata> {
+  const defaultLocale = 'en';
+  const translations = resources[defaultLocale];
+  
+  return {
+    title: translations.metadata.title,
+    description: translations.metadata.description,
+    keywords: translations.metadata.keywords,
+    authors: [{ name: "Júlia Klee" }],
+    metadataBase: new URL("https://emoto.juliaklee.wtf"),
+    openGraph: {
+      type: "website",
+      url: "https://emoto.juliaklee.wtf/",
+      title: translations.metadata.title,
+      description: translations.metadata.description,
+      images: ["/img/Emoto-Background.png"],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: translations.metadata.title,
+      description: translations.metadata.description,
+      images: ["/img/Emoto-Background.png"],
+    },
+    alternates: {
+      languages: {
+        'en': '/en',
+        'pt': '/pt'
+      }
+    }
+  };
+}
 
 export default function RootLayout({
   children,
